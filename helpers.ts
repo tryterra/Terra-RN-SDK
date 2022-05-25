b465a06d819982b7c55c90896e3206f73d58f94e
JaafarRammal <jarammal@gmail.com>
2022-05-25 14:50:21 +0100

    add helpers for enums

---
+++ helpers.ts
@@ -0,0 +1,13 @@
+export enum Permissions {
+    "ATHLETE",
+    "ACTIVITY",
+    "BODY",
+    "DAILY",
+    "NUTRITION",
+    "SLEEP",
+}
+
+export enum Connections {
+    "APPLE_HEALTH",
+    "FREESTYLE_LIBRE",
+}
